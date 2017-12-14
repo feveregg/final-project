a2d00be2d99f6097160cc84377701270b77e2e8d
feveregg <wk45@afsconnect2.njit.edu>
2017-12-14 15:22:09 -0500

    edit todos

--- collections/todos.php
+++ collections/todos.php
@@ -1,27 +1,20 @@
 <?php
-
 class todos extends database\collection
 {
-    protected static $modelName = 'todo';
+   
 
+    protected static $modelName = 'todo';
     //This is the function to write to find tasks by user ID for listing on their homepage.
     //Don't forget to return the record set see findAll in the collection class
     public static  function findTasksbyID($userid) {
-
-
         $tableName = get_called_class();
-        $sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid = ?';
-
-
+        $sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid ="' . $userid.'"';
+        //echo $sql;
         //grab the only record for find one and return as an object
-        $recordsSet = self::getResults($sql, $userid);
+        $recordsSet = self::getResults($sql);
+        return $recordsSet;
+  
 
-        if (is_null($recordsSet)) {
-            return FALSE;
-        } else {
-            return $recordsSet;
-        }
     }
 }
-
 ?>
