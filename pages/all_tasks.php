f7e9e63625155f744bc1ec0b109429cd2f6e3941
feveregg <wk45@afsconnect2.njit.edu>
2017-12-15 19:21:42 -0500

    edit a little

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -1,33 +1,35 @@
-<!DOCTYPE html>
-<html>
-  <head>
+<!doctype html>
+
+<html lang="en">
+<head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>WSD Project - FALL2017</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="all,follow">
-    <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-    <!-- Fontastic Custom icon font-->
-    <link rel="stylesheet" href="css/fontastic.css">
-    <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
-    <!-- Google fonts - Barlow -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Barlow:300,400,700">
-    <!-- theme stylesheet-->
-    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="css/custom.css">
-    <!-- Favicon-->
-    <link rel="shortcut icon" href="favicon.png">
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-  </head>
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<?php
+//this is how you print something
+
+print utility\htmlTable::genarateTableFromMultiArray($data);
 
 
-  
+?>
+
+<br>
+<br>
+
+
+
  <style>
     tab{
         float: right;
@@ -40,13 +42,13 @@
     
 <form action="index.php?page=tasks&action=insert" method="post">
 
-    Owneremail: <tab><input type="email" name="owneremail"></tab><br><br>
-    Ownerid:    <tab><input type="number" name="ownerid"></tab><br><br>
-    Createdate: <tab><input type="text" name="createddate"></tab><br><br>
-    Duedate:    <tab><input type="text" name="duedate"></tab><br><br>
-    Message:    <tab><input type="text" name="message"></tab><br><br>
+    Owneremail: <tab><input type="email" name="Email"></tab><br><br>
+    Ownerid:    <tab><input type="number" name="ID"></tab><br><br>
+    Createdate: <tab><input type="text" name="create time"></tab><br><br>
+    Duedate:    <tab><input type="text" name="due"></tab><br><br>
+    Message:    <tab><input type="text" name="content"></tab><br><br>
     Isdone:     <tab><input type="text" name="isdone"></tab><br><br>
-    <input type="submit" value="Insert Task">
+    <input type="submit" value="Insert">
 </form>
 
 
