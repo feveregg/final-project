9edf0b08666f15f19ec58e65e8fc0149c31a22b6
feveregg <wk45@afsconnect2.njit.edu>
2017-12-15 19:16:08 -0500

    remove some

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -27,63 +27,11 @@
   </head>
   <body>
     <div class="page home-page">
-      <!-- Main Navbar-->
-    <header class="header">
-        <nav class="navbar">
-          <!-- Search Box-->
-          <div class="search-box">
-            <button class="dismiss"><i class="icon-close"></i></button>
-            <form id="searchForm" action="#" role="search">
-              <input type="search" placeholder="What are you looking for..." class="form-control">
-            </form>
-          </div>
-          <div class="container-fluid">
-            <div class="navbar-holder d-flex align-items-center justify-content-between">
-              <!-- Navbar Header-->
-              <div class="navbar-header">
-                <!-- Navbar Brand --><a href="https://www.njit.edu/graduatestudies/degree-programs/graduatecertificates/web-systems-development-cert/" class="navbar-brand">
-                  <div class="brand-text brand-big"><span>Web System Development </span><strong> ToDo Task Admin </strong></div>
-                  <div class="brand-text brand-small"><strong>TD</strong></div></a>
-                <!-- Toggle Button--><a id="toggle-btn" href="#" class="menu-btn active"><span></span><span></span><span></span></a>
-              </div>
-              <!-- Navbar Menu -->
-              <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
-                <!-- Search-->
-                <li class="nav-item d-flex align-items-center"><a id="search" href="#"><i class="icon-search"></i></a></li>
-                <!-- Notifications
-                <!-- Messages                        -->
-
-                <li class="nav-item dropdown"> <a id="messages" rel="nofollow" data-target="#" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link"><i class="fa fa-envelope-o"></i><span class="badge bg-orange">10</span></a>
-                  <ul aria-labelledby="notifications" class="dropdown-menu">
-                    
-                    <li><a rel="nofollow" href="#" class="dropdown-item all-notifications text-center"> <strong>Read all messages    </strong></a></li>
-                  </ul>
-                </li>
-                <!-- Logout   -->
-                <li class="nav-item">
-
-                 <form action="index.php?page=accounts&action=logout" method="post" id="form3" style="float:right;">
-                 <li><button class="btn btn-link navbar-btn" type="submit">Logout</button></li>
-                 </form>
-                <!-- My Profile-->             
-                 <form action="index.php?page=accounts&action=myProf" method="post" id="form4" style="float:right;">
-                 <li><span class="glyphicon glyphicon-user"></span><button class="btn btn-link navbar-btn" type="submit">My Profile</button>
-                 </li>
-                 </form>                            
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
-      <div class="page-content d-flex align-items-stretch">
-        <!-- Side Navbar -->
-        <nav class="side-navbar">
-          <!-- Sidebar Header-->
-          <div class="sidebar-header d-flex align-items-center">
-            <div class="avatar"><img src="img/avatar-1.jpg" alt="" class="img-fluid rounded-circle"></div>
+     
+    
             <div class="title">
-              <h1 class="h4">Himanshu Hunge</h1>
-              <p>Web Designer</p>
+              <h1 class="h4">Final Project</h1>
+              
             </div>
           </div>
           <!-- Sidebar Navidation Menus-->
@@ -91,10 +39,16 @@
           <div>
            <ul class="list-unstyled">
             <form action="index.php?page=tasks&action=create" method="POST" style="float:left;">
-            <button class="btn btn-link navbar-btn" class="icon-interface-windows" type="submit"></i>Create Task</button>
+            <button class="btn btn-link navbar-btn" class="icon-interface-windows" type="submit"></i>Creat Todo</button>
             </form></ul>
           </div>
 
+       <br/>
+              <ul id="dashvariants" class="collapse list-unstyled">
+                <li><a href="https://web.njit.edu/~wk45/homework6/">Data Output</a></li>
+                
+              </ul>
+            </li>
             
            
         </nav>
