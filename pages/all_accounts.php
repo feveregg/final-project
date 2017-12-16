b539dec788bc5daa2cc5f5ae727eee73e2e8f14c
feveregg <wk45@afsconnect2.njit.edu>
2017-12-15 19:12:21 -0500

    edit little

--- pages/all_accounts.php
+++ pages/all_accounts.php
@@ -2,56 +2,23 @@
 <!DOCTYPE html>
 <html lang="en">
   
-  <body>
-    <div class="page home-page">
-      <!-- Main Navbar-->
-    <header class="header">
-        <nav class="navbar">
-          <!-- Search Box-->
-          <div class="search-box">
-            <button class="dismiss"><i class="icon-close"></i></button>
-            <form id="searchForm" action="#" role="search">
-              <input type="search" placeholder="What are you looking for..." class="form-control">
-            </form>
-          </div>
-          <div class="container-fluid">
-            <div class="navbar-holder d-flex align-items-center justify-content-between">
-              <!-- Navbar Header-->
-              <div class="navbar-header">
-                <!-- Navbar Brand --><a href="https://www.njit.edu/graduatestudies/degree-programs/graduatecertificates/web-systems-development-cert/" class="navbar-brand">
-                  <div class="brand-text brand-big"><span> WSD </span><strong> ToDo Task Admin </strong></div>
-                  <div class="brand-text brand-small"><strong>TD</strong></div></a>
-                <!-- Toggle Button--><a id="toggle-btn" href="#" class="menu-btn active"><span></span><span></span><span></span></a>
-              </div>
-              <!-- Navbar Menu -->
-              <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
-                <!-- Search-->
-                <li class="nav-item d-flex align-items-center"><a id="search" href="#"><i class="icon-search"></i></a></li>
-                <!-- Notifications
-                <!-- Messages                        -->
 
-                <li class="nav-item dropdown"> <a id="messages" rel="nofollow" data-target="#" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link"><i class="fa fa-envelope-o"></i><span class="badge bg-orange">10</span></a>
-                  <ul aria-labelledby="notifications" class="dropdown-menu">
-                    
-                    <li><a rel="nofollow" href="#" class="dropdown-item all-notifications text-center"> <strong>Read all messages    </strong></a></li>
-                  </ul>
-                </li>
-                <!-- Logout   -->
-                <li class="nav-item">
+   
+ <head>
+    <meta charset="utf-8">
 
-                 <form action="index.php?page=accounts&action=logout" method="post" id="form3" style="float:right;">
-                 <li><button class="btn btn-link navbar-btn" type="submit">Logout</button></li>
-                 </form>
-                <!-- My Profile-->             
-                 <form action="index.php?page=accounts&action=myProf" method="post" id="form4" style="float:right;">
-                 <li><span class="glyphicon glyphicon-user"></span><button class="btn btn-link navbar-btn" type="submit">My Profile</button>
-                 </li>
-                 </form>                            
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+
       <div class="page-content d-flex align-items-stretch">
         <!-- Side Navbar -->
         <nav class="side-navbar">
