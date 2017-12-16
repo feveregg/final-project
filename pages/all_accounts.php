c964949961e163cfc61155d3a43b23114a398fa4
feveregg <wk45@afsconnect2.njit.edu>
2017-12-15 19:14:53 -0500

    edit little

--- pages/all_accounts.php
+++ pages/all_accounts.php
@@ -20,9 +20,8 @@
 
 
      <body>
-<h1>All Accounts</h1>
+<h1>Accounts</h1>
 
-<h1> HI</h1>
 
 <?php
 //this is how you print something
@@ -38,20 +37,3 @@ print utility\htmlTable::genarateTableFromMultiArray($data);
 </html>
                 
          
-    <!-- Javascript files-->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="vendor/popper.js/umd/popper.min.js"> </script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
-    <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
-    <script src="js/charts-home.js"></script>
-    <script src="js/front.js"></script>
-  </body>
-</html>
-
-
-
-<script src="js/scripts.js"></script>
-</body>
-</html>
