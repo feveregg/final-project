6d9abb7ee1ffe85176e2bfaa4f1c075405360d62
feveregg <wk45@afsconnect2.njit.edu>
2017-12-15 19:33:22 -0500

    add some

--- pages/notfound.php
+++ pages/notfound.php
@@ -7,7 +7,9 @@
     <meta charset="utf-8">
 
 
-    <title> Final Project </title>
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
 
